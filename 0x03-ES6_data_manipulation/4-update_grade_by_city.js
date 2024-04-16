c63faf5b93dc174c6adebd500493651dddf95687
Sarahgad <sarahgad91@gmail.com>
2024-04-16 02:29:49 +0200

    4

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,11 +1,12 @@
 export default function updateStudentGradeByCity(getListStudents, city, newGrades) {
-  const specificArray = getListStudents.filter((student) => student.location === city);
-  const updateStudent = specificArray.map((student) => {
-    const newgrad = newGrades.find((grade) => grade.id === student.id);
-    if (newgrad) {
-      return { ...student, grade: newgrad.grade };
-    }
-    return student;
-  });
-  return updateStudent;
+  const specificArray = getListStudents
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const newgrad = newGrades.find((grade) => grade.studentId === student.id);
+      if (newgrad) {
+        return { ...student, grade: newgrad.grade };
+      }
+      return { ...student, grade: 'N/A' };
+    });
+  return specificArray;
 }
